d00d9cbd1c77fad7c14d54b8f920283e374be497
RYNO-X <73129419+RYNO-X@users.noreply.github.com>
2021-05-09 11:38:37 +0530

    Add files via upload

--- main/__main__.py
+++ main/__main__.py
@@ -4,16 +4,18 @@ import platform
 
 from Config.config import Config
 
-
+   
+    
+      # 1 BOT
 from telethon.sync import TelegramClient
 
 api_id = Config.API_ID
 api_hash = Config.API_HASH
 bot_token = Config.BOT_TOKEN1
 
-bot = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
+bot1 = TelegramClient('bot1', api_id, api_hash).start(bot_token=bot_token)
 
-async def main():
+async def main1():
 
         message = Config.MESSAGE
         chat = Config.CHAT_ID
@@ -21,8 +23,194 @@ async def main():
         counter = int(Config.TIME)
         spam_message = str(Config.MESSAGE)
         for i in range(1, counter):
-            await bot.send_message(chat, spam_message ,)
+            await bot1.send_message(chat, spam_message ,)
 
 
-with bot:
-    bot.loop.run_until_complete(main())
+with bot1:
+    bot1.loop.run_until_complete(main1())
+    
+   
+    
+     
+       
+   # 2 BOT
+api_id = Config.API_ID
+api_hash = Config.API_HASH
+bot_token = Config.BOT_TOKEN1
+
+bot2 = TelegramClient('bot2', api_id, api_hash).start(bot_token=bot_token)
+
+async def main2():
+
+        message = Config.MESSAGE
+        chat = Config.CHAT_ID
+        chat = int(Config.CHAT_ID)
+        counter = int(Config.TIME)
+        spam_message = str(Config.MESSAGE)
+        for i in range(1, counter):
+            await bot2.send_message(chat, spam_message ,)
+
+
+with bot2:
+    bot2.loop.run_until_complete(main2())
+    
+   
+    
+     
+       
+   #3 BOT
+from telethon.sync import TelegramClient
+
+api_id = Config.API_ID
+api_hash = Config.API_HASH
+bot_token = Config.BOT_TOKEN1
+
+bot3 = TelegramClient('bot3', api_id, api_hash).start(bot_token=bot_token)
+
+async def main3():
+
+        message = Config.MESSAGE
+        chat = Config.CHAT_ID
+        chat = int(Config.CHAT_ID)
+        counter = int(Config.TIME)
+        spam_message = str(Config.MESSAGE)
+        for i in range(1, counter):
+            await bot3.send_message(chat, spam_message ,)
+
+
+with bot3:
+    bot3.loop.run_until_complete(main3())
+
+
+
+
+  #4 BOT
+
+from telethon.sync import TelegramClient
+
+api_id = Config.API_ID
+api_hash = Config.API_HASH
+bot_token = Config.BOT_TOKEN1
+
+bot4 = TelegramClient('bot4', api_id, api_hash).start(bot_token=bot_token)
+
+async def main4():
+
+        message = Config.MESSAGE
+        chat = Config.CHAT_ID
+        chat = int(Config.CHAT_ID)
+        counter = int(Config.TIME)
+        spam_message = str(Config.MESSAGE)
+        for i in range(1, counter):
+            await bot4.send_message(chat, spam_message ,)
+
+
+with bot4:
+    bot4.loop.run_until_complete(main4())<
+
+
+
+
+ # 5 BOT
+from telethon.sync import TelegramClient
+
+api_id = Config.API_ID
+api_hash = Config.API_HASH
+bot_token = Config.BOT_TOKEN1
+
+bot5 = TelegramClient('bot5', api_id, api_hash).start(bot_token=bot_token)
+
+async def main5():
+
+        message = Config.MESSAGE
+        chat = Config.CHAT_ID
+        chat = int(Config.CHAT_ID)
+        counter = int(Config.TIME)
+        spam_message = str(Config.MESSAGE)
+        for i in range(1, counter):
+            await bot5.send_message(chat, spam_message ,)
+
+
+with bot5:
+    bot5.loop.run_until_complete(main5())
+    
+   
+    
+     
+       
+   # 6 BOT
+api_id = Config.API_ID
+api_hash = Config.API_HASH
+bot_token = Config.BOT_TOKEN1
+
+bot6 = TelegramClient('bot6', api_id, api_hash).start(bot_token=bot_token)
+
+async def main6():
+
+        message = Config.MESSAGE
+        chat = Config.CHAT_ID
+        chat = int(Config.CHAT_ID)
+        counter = int(Config.TIME)
+        spam_message = str(Config.MESSAGE)
+        for i in range(1, counter):
+            await bot6.send_message(chat, spam_message ,)
+
+
+with bot6:
+    bot6.loop.run_until_complete(main6())
+    
+   
+    
+     
+       
+   #7 BOT
+from telethon.sync import TelegramClient
+
+api_id = Config.API_ID
+api_hash = Config.API_HASH
+bot_token = Config.BOT_TOKEN1
+
+bot7 = TelegramClient('bot7', api_id, api_hash).start(bot_token=bot_token)
+
+async def main7():
+
+        message = Config.MESSAGE
+        chat = Config.CHAT_ID
+        chat = int(Config.CHAT_ID)
+        counter = int(Config.TIME)
+        spam_message = str(Config.MESSAGE)
+        for i in range(1, counter):
+            await bot7.send_message(chat, spam_message ,)
+
+
+with bot7:
+    bot7.loop.run_until_complete(main7())
+
+
+
+
+  #8 BOT
+
+from telethon.sync import TelegramClient
+
+api_id = Config.API_ID
+api_hash = Config.API_HASH
+bot_token = Config.BOT_TOKEN1
+
+bot8 = TelegramClient('bot8', api_id, api_hash).start(bot_token=bot_token)
+
+async def main8():
+
+        message = Config.MESSAGE
+        chat = Config.CHAT_ID
+        chat = int(Config.CHAT_ID)
+        counter = int(Config.TIME)
+        spam_message = str(Config.MESSAGE)
+        for i in range(1, counter):
+            await bot8.send_message(chat, spam_message ,)
+
+
+with bot8:
+    bot8.loop.run_until_complete(main8())
+
+
