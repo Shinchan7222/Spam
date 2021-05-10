8754dfd7d595dbc78e460fdbf205278ab5daa811
RYNO-X <73129419+RYNO-X@users.noreply.github.com>
2021-05-10 20:50:58 +0530

    Add files via upload

--- main/__main__.py
+++ main/__main__.py
@@ -74,27 +74,38 @@ bot_token = Config.BOT_TOKEN8
 
 bot8 = TelegramClient('bot8', api_id, api_hash).start(bot_token=bot_token)
 
-
-Hunter = [bot1, bot2, bot3, bot4, bot5, bot6, bot7, bot8]
-
-i = 0
-while i<10:
-async def main1(): 
-   for op in Hunter:
+i = 1
+while i<10
         message = Config.MESSAGE
         chat = Config.CHAT_ID
         chat = int(Config.CHAT_ID)
-        counter = int(Config.TIME)
         spam_message = str(Config.MESSAGE)
-        for i in range(1, counter):
-            await op.send_message(chat, spam_message ,)
+            await bot1.send_message(chat, spam_message ,)
+            await bot2.send_message(chat, spam_message ,)
+            await bot3.send_message(chat, spam_message ,)
+            await bot4.send_message(chat, spam_message ,)
+            await bot5.send_message(chat, spam_message ,)
+            await bot6.send_message(chat, spam_message ,)
+            await bot7.send_message(chat, spam_message ,)
+            await bot8.send_message(chat, spam_message ,)
+            
+with bot1:
+    bot1.run_until_disconnected()
+with bot2:
+    bot2.run_until_disconnected()
+with bot3:
+    bot3.run_until_disconnected()
+with bot4:
+    bot4.run_until_disconnected()
+with bot5:
+    bot5.run_until_disconnected()
+with bot6:
+    bot6.run_until_disconnected()
+with bot7:
+    bot7.run_until_disconnected()
+with bot8:
+    bot8.run_until_disconnected()
 
-
-with Hunter[0]:
-
-     Hunter[0].loop.run_until_complete(main1())
-
-          
 
 
      
